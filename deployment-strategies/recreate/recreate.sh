052cd5838bc55f7eae8763fb201b682c822b46d3
SANDEEP RAWAT <sandeep@opstree.com>
2021-04-11 14:18:38 +0530

    Added witing part

--- deployment-strategies/recreate/recreate.sh
+++ deployment-strategies/recreate/recreate.sh
@@ -11,12 +11,15 @@ function startDeployment() {
 function recreateDeployment() {
     setReplicaForRS my-app-v1 0
     apply_manifest ../rs2.yaml
+    setReplicaForRS my-app-v2 5
+    waitForRS my-app-v2 5
     replicaSetStatus
 }
 
 function rollback() {
     setReplicaForRS my-app-v2 0
     setReplicaForRS my-app-v1 5
+    waitForRS my-app-v1 5
     replicaSetStatus
 }
 
